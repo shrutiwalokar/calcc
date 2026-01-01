abf96cec398aed0b8294182aebfa65c750915f91
Shruti <shruti.walokar.it@ghrce.raisoni.net>
2026-01-01 07:54:34 +0530

    Added calc.java

---
+++ calc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+public class calc {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers: ");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.println("Addition: " + (num1+num2));
+		sc.close();
+	}
+}
